1fa86d96fe6b62c14d7637bf3c013e221aafe76f
Carlos Albaladejo Pérez <karlozalb@gmail.com>
2018-02-25 21:25:53 +0100

    First commit (trying to learn Kotlin).

---
+++ app/src/main/java/com/projectclean/easyhomeexpenses/adapters/ExpensesAdapter.kt
@@ -0,0 +1,28 @@
+package com.projectclean.easyhomeexpenses.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.projectclean.easyhomeexpenses.R
+import com.projectclean.easyhomeexpenses.models.Expense
+
+
+/**
+ * Created by Carlos Albaladejo Pérez on 24/02/2018.
+ */
+
+class ExpensesAdapter(var items : List<Expense>, var listener : (Expense) -> Unit) : RecyclerView.Adapter<ExpensesAdapter.ViewHolder>()
+{
+    class ViewHolder(var itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent.inflate(R.layout.expense_view))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position], listener)
+
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount() = items.size
+
+}
---
+++ app/src/main/java/com/projectclean/easyhomeexpenses/models/Expense.kt
@@ -0,0 +1,7 @@
+package com.projectclean.easyhomeexpenses.models
+
+/**
+ * Created by Carlos Albaladejo Pérez on 24/02/2018.
+ */
+
+data class Expense(var name: String)
