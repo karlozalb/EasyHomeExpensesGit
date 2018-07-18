21ef676bf0eb0519b937dc84a1cc2f8bc742ece7
Carlos <karlozalb@gmail.com>
2018-07-18 17:38:49 +0200

    EditExpenseActivity

--- app/src/main/java/com/projectclean/easyhomeexpenses/activities/EditExpenseActivity.kt
+++ app/src/main/java/com/projectclean/easyhomeexpenses/activities/EditExpenseActivity.kt
@@ -1,2 +1,29 @@
 package com.projectclean.easyhomeexpenses.activities
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.projectclean.easyhomeexpenses.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.new_expense_view.*
+
+class EditExpenseActivity : AppCompatActivity() {
+
+    companion object {
+        val OWNER_NAME : String = "OwnerName"
+        val NAME : String = "Name"
+        val MONEY = "Money"
+        val DATE = "Date"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_expense_view)
+        setSupportActionBar(toolbar)
+
+        newExpenseOwner.setText(intent.getStringExtra(OWNER_NAME))
+        newExpenseOwner.setText(intent.getStringExtra(NAME))
+        newExpenseOwner.setText(intent.getStringExtra(MONEY))
+        newExpenseOwner.setText(intent.getStringExtra(DATE))
+
+    }
+}
